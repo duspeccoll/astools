dd97b73359bd1a2580722187390ba47c18f7a786
jackflaps <kevin@jackflaps.net>
2018-03-27 09:37:18 -0600

    python script to get JSON for all objects of a certain data model

---
+++ reports.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# This script just logs you into your ArchivesSpace backend and prints a session ID.
+# You can copy it into whatever script you want to write to do useful things with your database.
+
+import configparser, requests, json, os, sys
+
+config = configparser.ConfigParser()
+config.read('local_settings.cfg')
+
+dictionary = {
+  'baseURL': config.get('ArchivesSpace', 'baseURL'),
+  'repository':config.get('ArchivesSpace', 'repository'),
+  'user': config.get('ArchivesSpace', 'user'),
+  'password': config.get('ArchivesSpace', 'password')
+}
+
+repositoryBaseURL = '{baseURL}/repositories/{repository}'.format(**dictionary)
+resourceURL = '{baseURL}'.format(**dictionary)
+
+auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
+session = auth['session']
+headers = {'X-ArchivesSpace-Session': session}
+
+data_models = { "1": "resources", "2": "archival_objects", "3": "agents", "4": "subjects", "5": "digital_objects", "6": "accessions", "7": "top_containers", "8": "container_profiles", "9": "events" }
+agent_types = ["people", "corporate_entities", "families", "software"]
+
+class Printer():
+    def __init__(self, data):
+        sys.stdout.write("\r\x1b[K"+data.__str__())
+        sys.stdout.flush
+
+def string_from_dict(msg, d):
+    print(msg)
+    for k in sorted(d):
+        print('* (' + k + ') ' + d[k])
+    s = input('> ')
+    if s in d:
+        return d[s]
+    else:
+        print("Please enter a value from the list.")
+        string_from_dict(msg, d)
+
+def run_report(jsonmodel):
+    request_url = "/%s" % jsonmodel
+    if jsonmodel == "subjects" or jsonmodel == "container_profiles" or jsonmodel.startswith("agents"):
+        request_url = resourceURL + request_url
+    else:
+        request_url = repositoryBaseURL + request_url
+
+    ids = requests.get(request_url + "?all_ids=true",headers=headers).json()
+    jsonmodel = jsonmodel.replace('agents/', '')
+    file_out = config.get('Destinations', 'home') + "/%s_report.json" % jsonmodel
+    f = open(file_out, "w")
+    f.write("{\"" + jsonmodel + "\":[")
+    for idx, id in enumerate(ids, start=1):
+        output = "Writing %s (%d of %d)... " % (jsonmodel, idx, len(ids))
+        Printer(output)
+        json = requests.get(request_url + "/" + str(id),headers=headers)
+        f.write(json.text.rstrip())
+        if idx < len(ids):
+            f.write(",")
+    f.write("]}")
+    f.close()
+    print("done!")
+
+jsonmodel = string_from_dict('Please select a data model:', data_models)
+
+if jsonmodel == "agents":
+    for atype in agent_types:
+        agent_type = "agents/" + atype
+        run_report(agent_type)
+else:
+    run_report(jsonmodel)
