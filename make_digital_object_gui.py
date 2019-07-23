8ccc5ffb641b5eb65023cdab573394eecf2ad2a5
alice.tarrant <alice.tarrant@du.edu>
2019-07-23 12:40:57 -0600

    Started adding functionality to gui elements

--- make_digital_object_gui.py
+++ make_digital_object_gui.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+from tkinter import filedialog
 from tkinter import ttk
+from make_digital_object import *
+from asnake.client.web_client import ASnakeAuthError
 
 
 class MainFrame(ttk.Frame):
@@ -7,23 +10,27 @@ class MainFrame(ttk.Frame):
         super(MainFrame, self).__init__(master)
         self.file_listbox = FileListbox(self)
         self.file_listbox.grid(column=0, row=0)
-        file_info_frame = FileInfoFrame(self)
+        file_info_frame = FileInfoFrame(self, self.file_listbox)
         file_info_frame.grid(column=0, row=1, sticky="W")
         item_listbox = tk.Listbox(self)
         item_listbox.grid(column=1, row=0)
 
 
 class FileInfoFrame(ttk.Frame):
-    def __init__(self, master):
+    def __init__(self, master, file_listbox):
         super(FileInfoFrame, self).__init__(master)
         self.file_path_entry = FilePathEntry(self)
         self.file_path_entry.grid(column=0, row=0, sticky="W")
         self.kaltura_id_entry = KalturaIDEntry(self)
         self.kaltura_id_entry.grid(column=0, row=1, sticky="W")
-        self.add_button = AddButton(self)
-        self.add_button.grid(column=1, row=0, sticky="SW")
+        self.add_button = AddButton(self, self.file_path_entry, self.kaltura_id_entry, file_listbox)
+        self.add_button.grid(column=1, row=0, sticky="W")
+        self.remove_button = RemoveButton(self, file_listbox)
+        self.remove_button.grid(column=1, row=1, sticky="W")
+        self.browse_button = BrowseButton(self, self.file_path_entry)
+        self.browse_button.grid(column=2, row=0)
         self.process_button = ProcessButton(self)
-        self.process_button.grid(column=1, row=1, sticky="SW")
+        self.process_button.grid(column=2, row=1, sticky="W")
 
 
 class FilePathEntry(ttk.Frame):
@@ -34,6 +41,13 @@ class FilePathEntry(ttk.Frame):
         self.entry = tk.Entry(self)
         self.entry.grid(column=0, row=1, sticky="W")
 
+    def get(self):
+        return self.entry.get()
+
+    def set(self, value):
+        self.entry.delete(0, 'end')
+        self.entry.insert(9, value)
+
 
 class KalturaIDEntry(ttk.Frame):
     def __init__(self, master):
@@ -43,15 +57,45 @@ class KalturaIDEntry(ttk.Frame):
         self.entry = tk.Entry(self)
         self.entry.grid(column=0, row=1, sticky='W')
 
+    def get(self):
+        return self.entry.get()
+
 
 class AddButton(tk.Button):
-    def __init__(self, master):
-        super(AddButton, self).__init__(master, text="Add")
+    def __init__(self, master, file_path_entry, kaltura_id_entry, file_listbox):
+        super(AddButton, self).__init__(master, text="Add", command=self._button_command)
+        self.file_path_entry = file_path_entry
+        self.kaltura_id_entry = kaltura_id_entry
+        self.file_listbox = file_listbox
+
+    def _button_command(self):
+        file_path = self.file_path_entry.get()
+        kaltura_id = self.kaltura_id_entry.get()
+        if file_path != "":
+            self.file_listbox.insert('', 'end', text=file_path, values=(kaltura_id,))
+            uri = check_uri_txt(file_path)
+            print(uri)
 
 
 class RemoveButton(tk.Button):
-    def __init__(self, master):
-        super(RemoveButton, self).__init__(master, text="Remove")
+    def __init__(self, master, file_listbox):
+        super(RemoveButton, self).__init__(master, text="Remove", command=self._button_command)
+        self.file_listbox = file_listbox
+
+    def _button_command(self):
+        index = self.file_listbox.selection()
+        if len(index) > 0:
+            self.file_listbox.delete(index)
+            
+            
+class BrowseButton(tk.Button):
+    def __init__(self, master, file_path_entry):
+        super(BrowseButton, self).__init__(master, text="Browse", command=self._button_command)
+        self.file_path_entry = file_path_entry
+
+    def _button_command(self):
+        dirname = filedialog.askdirectory(initialdir=r"R:\Archives Processing")
+        self.file_path_entry.set(dirname)
 
 
 class ProcessButton(tk.Button):
@@ -61,7 +105,8 @@ class ProcessButton(tk.Button):
 
 class FileListbox(ttk.Treeview):
     def __init__(self, master):
-        super(FileListbox, self).__init__(master, columns=("File Path", "Kaltura ID"))
+        super(FileListbox, self).__init__(master, columns=("kaltura_id",))
+        self.heading('kaltura_id', text="Kaltura ID")
 
 
 def setup_gui(root):
@@ -70,6 +115,11 @@ def setup_gui(root):
 
 
 def main():
+    try:
+        global AS
+        AS = ASpace()
+    except ASnakeAuthError:
+        print("Could not connect to ArchivesSpace.", file=sys.stderr)
     root = tk.Tk()
     root.title("Make Digital Object")
     setup_gui(root)
